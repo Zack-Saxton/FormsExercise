41dc9de7369bcf2281e99a601afa54a6a3c8d407
zsaxton261 <zack.saxton261@gmail.com>
2021-01-26 09:21:22 -0600

    fixed form field range

--- text.js
+++
@@ -1,6 +0,0 @@
-if(form.pwd1.value != "" && form.pwd1.value == form.pwd2.value) {
-    if(form.pwd1.value.length 5 < 10) {
-      alert("Error: Password must contain at least six characters!");
-      form.pwd1.focus();
-      return false;
-
